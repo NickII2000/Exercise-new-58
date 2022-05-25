cc8a0883ef87f22e2b5dd8c006c0130837a90586
Nick <Nick_II2000@mail.ru>
2022-05-25 20:59:17 +0600

    2-th commit

--- script.js
+++ script.js
@@ -12,5 +12,12 @@ const user = {
     // [line]: 777,
 };
 
-// console.log(Object.getOwnPropertyDescriptor(user, 'name'));
+for (const key in user) {
+    console.log(user[key]);
+}
 
+const arr = ['b', 'a', 'c'];
+
+for (const key in arr) {
+    console.log(arr[key]);
+}
