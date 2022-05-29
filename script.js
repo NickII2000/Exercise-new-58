bf64aa952a3bd44c91eb14bf3e01abdf1ede4728
Nick <Nick_II2000@mail.ru>
2022-05-29 22:16:24 +0600

    21-th commit

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ console.log(iterator.next());
 console.log(iterator.next());
 console.log(iterator.next());
 console.log(iterator.next());
-// console.log(iterator.next());
+console.log(iterator.next());
 
 // for (const res of salaries) {
 //     console.log(res);
